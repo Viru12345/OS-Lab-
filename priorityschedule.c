a69c44b1f6cbd9af27e31d5447635ef2da94e071
Mahavir Nahata 1BM21CS100 <117442487+Viru12345@users.noreply.github.com>
2023-08-28 00:00:33 +0530

    Update priorityschedule.c

--- priorityschedule.c
+++ priorityschedule.c
@@ -1,79 +1,56 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+ 
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
 int main()
 {
-    int et[20],at[10],n,i,j,temp,p[10],st[10],ft[10],wt[10],ta[10];
-    int totwt=0,totta=0;
-    float awt,ata;
-    char pn[10][10],t[10];
-    //clrscr();
-    printf("Enter the number of process:");
+    int n;
+    printf("Enter Number of Processes: ");
     scanf("%d",&n);
-    for(i=0; i<n; i++)
+ 
+    int b[n],p[n],index[n];
+    for(int i=0;i<n;i++)
     {
-        printf("Enter process name,arrivaltime,execution time & priority:");
-        //flushall();
-        scanf("%s%d%d%d",pn[i],&at[i],&et[i],&p[i]);
+        printf("Enter Burst Time and Priority Value for Process %d: ",i+1);
+        scanf("%d %d",&b[i],&p[i]);
+        index[i]=i+1;
     }
-    for(i=0; i<n; i++)
-        for(j=0; j<n; j++)
+    for(int i=0;i<n;i++)
+    {
+        int a=p[i],m=i;
+ 
+        for(int j=i;j<n;j++)
         {
-            if(p[i]<p[j])
+            if(p[j] > a)
             {
-                temp=p[i];
-                p[i]=p[j];
-                p[j]=temp;
-                temp=at[i];
-                at[i]=at[j];
-                at[j]=temp;
-                temp=et[i];
-                et[i]=et[j];
-                et[j]=temp;
-                strcpy(t,pn[i]);
-                strcpy(pn[i],pn[j]);
-                strcpy(pn[j],t);
+                a=p[j];
+                m=j;
             }
         }
-    for(i=0; i<n; i++)
-
-    {
-
-        if(i==0)
-        {
-            st[i]=at[i];
-            wt[i]=st[i]-at[i];
-            ft[i]=st[i]+et[i];
-            ta[i]=ft[i]-at[i];
-        }
-        else
-        {
-            st[i]=ft[i-1];
-            wt[i]=st[i]-at[i];
-            ft[i]=st[i]+et[i];
-            ta[i]=ft[i]-at[i];
-        }
-        totwt+=wt[i];
-        totta+=ta[i];
+ 
+        swap(&p[i], &p[m]);
+        swap(&b[i], &b[m]);
+        swap(&index[i],&index[m]);
     }
-    awt=(float)totwt/n;
-    ata=(float)totta/n;
-    printf("\nPname\tarrivaltime\texecutiontime\tpriority\twaitingtime\ttatime");
-    for(i=0; i<n; i++)
-        printf("\n%s\t%5d\t\t%5d\t\t%5d\t\t%5d\t\t%5d",pn[i],at[i],et[i],p[i],wt[i],ta[i]);
-    printf("\nAverage waiting time is:%f",awt);
-    printf("\nAverage turnaroundtime is:%f",ata);
+    
+    int t=0;
+    printf("\nOrder of process Execution is\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("P%d is executed from %d to %d\n",index[i],t,t+b[i]);
+        t+=b[i];
+    }
+    printf("\n");
+    printf("Process Id     Burst Time   Wait Time    TurnAround Time\n");
+    int wait_time=0;
+    for(int i=0;i<n;i++)
+    {
+        printf("P%d \t\t %d\t\t %d\t\t %d\n",index[i],b[i],wait_time,wait_time + b[i]);
+        wait_time += b[i];
+    }
     return 0;
 }
-OUTPUT:
-
-Enter the number of process:3
-Enter process name,arrivaltime,execution time & priority:1 2 1 3
-Enter process name,arrivaltime,execution time & priority:3 5 4 1
-Enter process name,arrivaltime,execution time & priority:3 4 6 2
-
-Pname   arrivaltime     executiontime   priority        waitingtime     tatime
-3           5               4               1               0               4
-3           4               6               2               5              11
-1           2               1               3              13              14
-Average waiting time is:6.000000
-Average turnaroundtime is:9.666667
