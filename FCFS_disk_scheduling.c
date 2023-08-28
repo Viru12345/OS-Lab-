36f7a540f0ee3bfbaba14b28028db32d740b3daa
Mahavir Nahata 1BM21CS100 <117442487+Viru12345@users.noreply.github.com>
2023-08-28 09:57:28 +0530

    Update FCFS_disk_scheduling.c

--- FCFS_disk_scheduling.c
+++ FCFS_disk_scheduling.c
@@ -1,27 +1,28 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-    int RQ[100],i,n,TotalHeadMoment=0,initial;
-    printf("Enter the number of Requests\n");
-    scanf("%d",&n);
-    printf("Enter the Requests sequence\n");
-    for(i=0;i<n;i++)
-     scanf("%d",&RQ[i]);
-    printf("Enter initial head position\n");
-    scanf("%d",&initial);
-    
-    // logic for FCFS disk scheduling
-    
-    for(i=0;i<n;i++)
-    {
-        TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
-        initial=RQ[i];
-    }
-    
-    printf("Total head moment is %d",TotalHeadMoment);
-    return 0;
-    
+            int queue[20],n,head,i,j,k,seek=0,max,diff;
+            float avg;
+            printf("Enter the max range of disk\n");
+            scanf("%d",&max);
+            printf("Enter the size of queue request\n");
+            scanf("%d",&n);
+            printf("Enter the queue of disk positions to be read\n");
+            for(i=1;i<=n;i++)
+            scanf("%d",&queue[i]);
+            printf("Enter the initial head position\n");
+            scanf("%d",&head);
+            queue[0]=head;
+            for(j=0;j<=n-1;j++)
+            {
+                        diff=abs(queue[j+1]-queue[j]);
+                        seek+=diff;
+                        printf("Disk head moves from %d to %d with seek                                                                                       %d\n",queue[j],queue[j+1],diff);
+            }
+            printf("Total seek time is %d\n",seek);
+            avg=seek/(float)n;
+            printf("Average seek time is %f\n",avg);
+            return 0;
 }
 Output:-
 
