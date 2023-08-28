8e3a2b0ea44130e003c7fbce96b5e31c7cd72923
Mahavir Nahata 1BM21CS100 <117442487+Viru12345@users.noreply.github.com>
2023-08-28 09:11:25 +0530

    Update first_fit.c

--- first_fit.c
+++ first_fit.c
@@ -1,38 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void implimentFirstFit(int blockSize[], int blocks, int processSize[], int processes)
+{
+    // This will store the block id of the allocated block to a process
+    int allocate[processes];
+    int occupied[blocks];
+
+    // initially assigning -1 to all allocation indexes
+    // means nothing is allocated currently
+    for(int i = 0; i < processes; i++)
+	{
+		allocate[i] = -1;
+	}
+	
+	for(int i = 0; i < blocks; i++){
+        occupied[i] = 0;
+    }
+	
+    // take each process one by one and find
+    // first block that can accomodate it
+    for (int i = 0; i < processes; i++)
+    {
+        for (int j = 0; j < blocks; j++) 
+        { 
+        if (!occupied[j] && blockSize[j] >= processSize[i])
+            {
+                // allocate block j to p[i] process
+                allocate[i] = j;
+                occupied[j] = 1;
+ 
+                break;
+            }
+        }
+    }
+
+    printf("\nProcess No.\tProcess Size\tBlock no.\n");
+    for (int i = 0; i < processes; i++)
+    {
+        printf("%d \t\t\t %d \t\t\t", i+1, processSize[i]);
+        if (allocate[i] != -1)
+            printf("%d\n",allocate[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+}
+
 void main()
 {
-	int bsize[10], psize[10], bno, pno, flags[10], allocation[10], i, j;
-	for(i = 0; i < 10; i++)
-	{
-		flags[i] = 0;
-		allocation[i] = -1;
-	}
-	printf("Enter no. of blocks: ");
-	scanf("%d", &bno);
-	printf("\nEnter size of each block: ");
-	for(i = 0; i < bno; i++)
-		scanf("%d", &bsize[i]);
-	printf("\nEnter no. of processes: ");
-	scanf("%d", &pno);
-	printf("\nEnter size of each process: ");
-	for(i = 0; i < pno; i++)
-		scanf("%d", &psize[i]);
-	for(i = 0; i < pno; i++)         //allocation as per first fit
-		for(j = 0; j < bno; j++)
-			if(flags[j] == 0 && bsize[j] >= psize[i])
-			{
-				allocation[j] = i;
-				flags[j] = 1;
-				break;
-			}
-	//display allocation details
-	printf("\nBlock no.\tsize\t\tprocess no.\t\tsize");
-	for(i = 0; i < bno; i++)
-	{
-		printf("\n%d\t\t%d\t\t", i+1, bsize[i]);
-		if(flags[i] == 1)
-			printf("%d\t\t\t%d",allocation[i]+1,psize[allocation[i]]);
-		else
-			printf("Not allocated");
-	}
+    int blockSize[] = {30, 5, 10};
+    int processSize[] = {10, 6, 9};
+    int m = sizeof(blockSize)/sizeof(blockSize[0]);
+    int n = sizeof(processSize)/sizeof(processSize[0]);
+    
+    implimentFirstFit(blockSize, m, processSize, n);
 }
+Output
+Process No.	Process Size	Block no.
+1 		10 		1
+2 		6 		3
+3 		9 		Not Allocated
